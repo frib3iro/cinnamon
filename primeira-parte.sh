571adf68e618b4b6839e28fb841b009e82493b02
Fabio <rib3iro@live.com>
2020-05-20 20:01:01 +0000

    Script para instalação do Arch Linux

--- primeira-parte.sh
+++ primeira-parte.sh
@@ -21,76 +21,74 @@ clear
 
 # Definindo layout do teclado
 echo -e "${seta} ${blue}Definir o layout do teclado${end}"
-sleep 1s
 loadkeys br-abnt2
+sleep 1s
 clear
 
 # Definir o idioma do ambiente live
 echo -e "${seta} ${blue}Definir o idioma do ambiente live${end}"
-sleep 1s
 sed -i 's/en_US ISO-8859-1/#en_US ISO-8859-1/' /etc/locale.gen
 sed -i 's/en_US.UTF-8/#en_US.UTF-8/' /etc/locale.gen
 sed -i 's/#pt_BR.UTF-8/pt_BR.UTF-8/' /etc/locale.gen
 sed -i 's/#pt_BR ISO-8859-1/pt_BR ISO-8859-1/' /etc/locale.gen
+sleep 1s
 clear
 
 echo -e "${seta} ${blue}Gerando locale-gen${end}"
-sleep 1s
 locale-gen
+sleep 1s
 clear
 
 echo -e "${seta} ${blue}Exportando a variável LANG${end}"
-sleep 1s
 export LANG=pt_BR.UTF-8
+sleep 1s
 clear
 
 echo -e "${seta} ${blue}Atualizando o relógio do sistema${end}"
-sleep 1s
 timedatectl set-ntp true
+sleep 1s
 clear
 
 echo -e "${seta} ${blue}Listando os discos${end}"
-sleep 1s
 lsblk -l | grep disk
+sleep 1s
 echo ""
 
 echo -en "${seta} ${blue}Informe o nome do seu disco: ${end}"
-sleep 1s
 read disco
 disco=/dev/${disco}
 clear
 
 # Iniciando particionamento
 echo -e "${seta} ${blue}Iniciando particionamento${end}"
+#(echo d; echo ""; echo d; echo ""; echo g; echo n; echo ""; echo ""; echo +512MB; echo t; echo 1; echo n; echo ""; echo ""; echo ""; echo w) | fdisk ${disco}
+fdisk ${disco}
 sleep 2s
-(echo d; echo ""; echo d; echo ""; echo g; echo n; echo ""; echo ""; echo +512MB; echo t; echo 1; echo n; echo ""; echo ""; echo ""; echo w) | fdisk ${disco}
 clear
 
 # Formatando partições
 echo -e "${seta} ${blue}Formatando as partições${end}"
-sleep 1s
 mkfs.fat -F32 ${disco}1
 mkfs.ext4 ${disco}2
+sleep 1s
 clear
 
 # Montando partições
 echo -e "${seta} ${blue}Montando as partições${end}"
-sleep 1s
 mount ${disco}2 /mnt
 mkdir -p /mnt/boot/EFI
 mount ${disco}1 /mnt/boot/EFI
+sleep 1s
 clear
 
 # Listando partições
 echo -e "${seta} ${blue}Conferindo as partições${end}"
-sleep 1s
 lsblk ${disco}
 sleep 5s
 clear
 
 # Configurando mirrorlist
 echo -e "${seta} ${blue}Fazendo backup do mirrorlist${end}"
-sleep 1s
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 clear
 echo -e "${seta} ${blue}Comentando todos os servidores${end}"
@@ -100,26 +98,27 @@ clear
 echo -e "${seta} ${blue}Descomentando os servidores Brasileiros${end}"
 sleep 1s
 sed '/Brazil/{n;s/^#//}' ${mirror}.bkp > ${mirror}
+sleep 1s
 clear
 
 echo -e "${seta} ${blue}Atualizando os repositórios${end}"
-sleep 1s
 pacman -Syyy
+sleep 1s
 clear
 
 echo -e "${seta} ${blue}Instalar os pacotes base${end}"
-sleep 1s
 pacstrap /mnt base base-devel linux linux-firmware
+sleep 1s
 clear
 
 echo -e "${seta} ${blue}Gerando o fstab${end}"
-sleep 1s
 genfstab -U /mnt >> /mnt/etc/fstab
+sleep 1s
 clear
 
 echo -e "${seta} ${blue}Copiando o script archinstall-02.sh para /mnt${end}"
-sleep 1s
 cp segunda-parte.sh /mnt
+sleep 1s
 clear
 
 echo -e "${seta} ${blue}Iniciando arch-chroot${end}"
--- terceira-parte.sh
+++ terceira-parte.sh
@@ -16,92 +16,92 @@ sleep 1s
 clear
 
 echo -e "${seta} ${blue}Atualizando os repositórios${end}"
-sleep 1s
 echo $senha | sudo -S pacman -Syyy
+sleep 1s
 clear
 
 # Drivers de video
 echo -e "$seta ${blue}Instalando os drivers de vídeo${end}"
-sleep 1s
 echo $senha | sudo -S pacman -S xf86-video-intel intel-ucode --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando o cinnamon desktop${end}"
-sleep 1s
 echo $senha | sudo -S pacman -S cinnamon cinnamon-translations cinnamon-screensaver cinnamon-session cinnamon-settings-daemon --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando o nemo${end}"
-sleep 1s
 echo $senha | sudo -S pacman -S nemo nemo-preview nemo-share nemo-fileroller --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando compactadores de arquivos${end}"
-sleep 1s
 echo $senha | sudo -S pacman -S p7zip unrar unzip zip bzip2 --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando pacotes necessários${end}"
+echo $senha | sudo -S pacman -S xorg man-db metacity mtools neofetch bolt gst-libav xdg-utils xdg-user-dirs archlinux-wallpaper system-config-printer dialog youtube-dl xf86-input-synaptics gimp libreoffice libreoffice-fresh-pt-br virtualbox virtualbox-guest-utils bash-completion bluez bluez-cups bluez-tools alsa-utils blueberry  gnome-bluetooth gnome-calculator gnome-calendar archlinux-keyring gnome-menus gnome-online-accounts gnome-power-manager gnome-screenshot gnome-settings-daemon gnupg gufw ufw accountsservice xreader gnome-disk-utility xfce4-terminal xterm rsync tcpdump colord coreutils cpio ttf-hack neofetch cmatrix system-config-printer gnome-keyring gnome-system-monitor baobab htop lollypop nemo-python ffmpegthumbnailer --noconfirm
 sleep 1s
-echo $senha | sudo -S pacman -S xorg man-db metacity mtools neofetch bolt gst-libav xdg-utils xdg-user-dirs archlinux-wallpaper system-config-printer dialog youtube-dl xf86-input-synaptics gimp libreoffice libreoffice-fresh-pt-br virtualbox virtualbox-guest-utils bash-completion bluez bluez-cups bluez-tools alsa-utils blueberry  gnome-bluetooth gnome-calculator gnome-calendar archlinux-keyring gnome-menus gnome-online-accounts gnome-power-manager gnome-screenshot gnome-settings-daemon gnupg gufw ufw accountsservice xreader gnome-disk-utility xfce4-terminal xterm rsync tcpdump colord coreutils cpio ttf-hack neofetch cmatrix system-config-printer gnome-keyring gnome-system-monitor baobab htop lollypop nemo-python  ffmpegthumbnailer --noconfirm
 clear
 
 echo -e "$seta ${blue}Instalando o yay${end}"
-sleep 1s
 git clone https://aur.archlinux.org/yay.git
 cd yay/
 makepkg -si PKGBUILD --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando cinnamon-sound-effects${end}"
-sleep 1s
 yay -S cinnamon-sound-effects --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando o xviewer${end}"
-sleep 1s
 yay -S xviewer xviewer-plugins --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando as fontes${end}"
-sleep 1s
 yay -S ttf-ms-fonts --noconfirm
 yay -S ttf-ubuntu-font-family --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando o mint-themes${end}"
-sleep 1s
 yay -S mint-themes mint-y-icons mint-x-icons mint-backgrounds mintlocale --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando o timeshift${end}"
-sleep 1s
 yay -S timeshift --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando o xcursor-breeze${end}"
-sleep 1s
 yay -Ss xcursor-breeze --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando o cinnamon-sound-effects${end}"
-sleep 1s
 yay -S cinnamon-sound-effects --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando o gnome-online-accounts-git${end}"
-sleep 1s
 yay -S gnome-online-accounts-git --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando o google-chrome${end}"
-sleep 1s
 yay -S google-chrome --noconfirm
+sleep 1s
 clear
 
 echo -e "$seta ${blue}Instalando o 4kvideodownloader${end}"
-sleep 1s
 yay -S 4kvideodownloader --noconfirm
+sleep 1s
 clear
 # echo -e "${seta} ${blue}Definindo o layout do teclado no ambiente cinnamon${end}"
 # sleep 1s
@@ -128,17 +128,18 @@ else
     exit 1
 clear
 
-#echo -e "$seta ${blue}Instalando e iniciando o lightdm${end}"
-#sleep 1s
-#echo $senha | sudo -S pacman -S lightdm
-#yay -S lightdm lightdm-settings lightdm-slick-greeter
-#echo $senha | sudo -Ssudo systemctl enable lightdm 
-#clear
-
-echo -e "$seta ${blue}Instalando e iniciando o gdm${end}"
+echo -e "$seta ${blue}Instalando e iniciando o lightdm${end}"
+echo $senha | sudo -S pacman -S lightdm
+yay -S lightdm-settings lightdm-slick-greeter
+echo $senha | sudo -S systemctl enable lightdm 
+echo $senha | sudo -S systemctl start lightdm 
 sleep 1s
-echo $senha | sudo -S pacman -S gdm
-echo $senha | sudo -S sudo systemctl enable gdm
-echo $senha | sudo -S sudo systemctl start gdm
 clear
+
+# echo -e "$seta ${blue}Instalando e iniciando o gdm${end}"
+# sleep 1s
+# echo $senha | sudo -S pacman -S gdm
+# echo $senha | sudo -S systemctl enable gdm
+# echo $senha | sudo -S systemctl start gdm
+# clear
 
