58bfd9d1295628c21ff17613c5cb1d530dfc1c32
Fabio <rib3iro@live.com>
2020-05-29 13:05:19 -0300

    Commit básico

--- terceira-parte.sh
+++ terceira-parte.sh
@@ -11,7 +11,7 @@ seta='\e[32;1m==>\e[m'
 
 # Tela de boas vindas
 clear
-echo -e "$seta ${blue}Bem vindo a terceira parte da instalação!${end}"
+echo -e "${seta} ${blue}Bem vindo a terceira parte da instalação!${end}"
 sleep 2s
 clear
 
@@ -21,22 +21,22 @@ sleep 2s
 clear
 
 # Drivers de video
-echo -e "$seta ${blue}Instalando os drivers de vídeo${end}"
+echo -e "${seta} ${blue}Instalando os drivers de vídeo${end}"
 echo $senha | sudo -S pacman -S nvidia nvidia-utils intel-ucode --noconfirm
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando o cinnamon desktop${end}"
+echo -e "${seta} ${blue}Instalando o cinnamon desktop${end}"
 echo $senha | sudo -S pacman -S cinnamon cinnamon-translations cinnamon-screensaver cinnamon-session cinnamon-settings-daemon --noconfirm
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando o nemo${end}"
+echo -e "${seta} ${blue}Instalando o nemo${end}"
 echo $senha | sudo -S pacman -S nemo nemo-preview nemo-share nemo-fileroller --noconfirm
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando pacotes necessários${end}"
+echo -e "${seta} ${blue}Instalando pacotes necessários${end}"
 echo $senha | sudo -S pacman -S accountsservice alsa-utils archlinux-keyring archlinux-wallpaper baobab bash-completion blueberry bluez bluez-cups bluez-tools bolt cmatrix colord coreutils cpio cronie dialog ffmpegthumbnailer gimp gnome-bluetooth gedit gnome-calculator gnome-calendar gnome-disk-utility gnome-keyring gnome-menus gnome-online-accounts gnome-power-manager gnome-screenshot gnome-settings-daemon gnome-system-monitor gnupg gst-libav gufw htop libreoffice libreoffice-fresh-pt-br lollypop man-db metacity mtools neofetch pass rsync system-config-printer tcpdump totem ttf-hack gnu-free-fonts ttf-dejavu ttf-nerd-fonts-symbols ufw unrar xdg-user-dirs xdg-utils xf86-input-synaptics gnome-terminal xcursor-vanilla-dmz-aa xclip xreader youtube-dl --noconfirm
 sleep 2s
 clear
@@ -62,7 +62,7 @@ echo $senha | sudo -S virsh net-autostart --network default
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando o yay${end}"
+echo -e "${seta} ${blue}Instalando o yay${end}"
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
@@ -70,87 +70,87 @@ sleep 2s
 clear
 
 # Hackerman -------------------------------------------------------
-echo -e "$seta ${blue}Instalando aircrack-ng e usbutils${end}"
+echo -e "${seta} ${blue}Instalando aircrack-ng e usbutils${end}"
 echo $senha | sudo -S pacman -S aircrack-ng usbutils --noconfirm
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando o crunch${end}"
+echo -e "${seta} ${blue}Instalando o crunch${end}"
 yay -S --noconfirm crunch
 sleep 2s
 clear
 # Hackerman -------------------------------------------------------
 
-echo -e "$seta ${blue}Instalando o kid3-cli para o lollypop${end}"
+echo -e "${seta} ${blue}Instalando o kid3-cli para o lollypop${end}"
 yay -S --noconfirm kid3-cli
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando os${end} ${yellow}firmwares warnigs${end} ${blue}do archlinux${end}"
+echo -e "${seta} ${blue}Instalando os${end} ${yellow}firmwares warnigs${end} ${blue}do archlinux${end}"
 yay -S --noconfirm aic94xx-firmware wd719x-firmware
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando o debtap${end}"
+echo -e "${seta} ${blue}Instalando o debtap${end}"
 yay -S --noconfirm debtap
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando gnome-terminal-transparency${end}"
+echo -e "${seta} ${blue}Instalando gnome-terminal-transparency${end}"
 yay -S --noconfirm gnome-terminal-transparency
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando cinnamon-sound-effects${end}"
+echo -e "${seta} ${blue}Instalando cinnamon-sound-effects${end}"
 yay -S --noconfirm cinnamon-sound-effects
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando spotify${end}"
+echo -e "${seta} ${blue}Instalando spotify${end}"
 yay -S --noconfirm spotify
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando o xviewer${end}"
+echo -e "${seta} ${blue}Instalando o xviewer${end}"
 yay -S --noconfirm xviewer xviewer-plugins
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando as fontes${end}"
+echo -e "${seta} ${blue}Instalando as fontes${end}"
 yay -S --noconfirm ttf-ms-fonts
 yay -S --noconfirm ttf-ubuntu-font-family
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando o mint-themes${end}"
+echo -e "${seta} ${blue}Instalando o mint-themes${end}"
 yay -S --noconfirm mint-themes mint-y-icons mintlocale
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando o gnome-online-accounts-git${end}"
+echo -e "${seta} ${blue}Instalando o gnome-online-accounts-git${end}"
 yay -S --noconfirm gnome-online-accounts-git
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando o google-chrome${end}"
+echo -e "${seta} ${blue}Instalando o google-chrome${end}"
 yay -S --noconfirm google-chrome
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Iniciando o xdg-update${end}"
+echo -e "${seta} ${blue}Iniciando o xdg-update${end}"
 sleep 2s
 if xdg-user-dirs-update; then
-    echo -e "$seta ${green}xdg-update iniciado com sucesso!${end}"
+    echo -e "${seta} ${green}xdg-update iniciado com sucesso!${end}"
     sleep 2s
     clear
 else
-    echo -e "$seta ${red}não foi possível iniciar o xdg-update!${end}"
+    echo -e "${seta} ${red}não foi possível iniciar o xdg-update!${end}"
     sleep 2s
     clear
     continue 
 fi
 
-echo -e "$seta ${blue}Instalando e iniciando o gdm${end}"
+echo -e "${seta} ${blue}Instalando e iniciando o gdm${end}"
 sleep 2s
 echo $senha | sudo -S pacman -S gdm --noconfirm
 echo $senha | sudo -S systemctl enable gdm
