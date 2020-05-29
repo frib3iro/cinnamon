3896a2bce86403205c50f0b5a0e2482f632c3bc0
Fabio <rib3iro@live.com>
2020-05-28 22:37:48 -0300

    Commit básico

--- terceira-parte.sh
+++ terceira-parte.sh
@@ -9,56 +9,81 @@ yellow='\e[33;1m'
 end='\e[m'
 seta='\e[32;1m==>\e[m'
 
+# Funções ------------------------------------------------------------
+driver_virtmanager(){
+    # Drivers de video para virt-manager
+    echo -e "$seta ${blue}Instalando os drivers de vídeo${end}"
+    echo $senha | sudo -S pacman -S xf86-video-qxl --noconfirm
+    sleep 2s
+    clear
+}
+
+driver_nvidia(){
+    # Drivers de video nvidia
+    echo -e "$seta ${blue}Instalando os drivers de vídeo nvidia${end}"
+    echo $senha | sudo -S pacman -S nvidia nvidia-utils nvidia-settings intel-ucode --noconfirm
+    sleep 2s
+    clear
+}
+
+instalar_lightdm(){
+    echo $senha | sudo -S pacman -S lightdm
+}
+
+deepin_greeter(){
+    yay -S lightdm-deepin-greeter
+}
+
+gtk_greeter(){
+    echo $senha | sudo -S pacman -S lightdm-gtk-greeter-settings
+}
+
+arquivo_de_configuracao(){
+    sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-slick-greeter/' /etc/lightdm/lightdm.conf
+}
+
+iniciar_testmode(){
+    lightdm --test-mode --debug
+}
+
+iniciar_servico(){
+    systemctl start lightdm.service && systemctl enable lightdm.service
+}
+# --------------------------------------------------------------------
+
 # Tela de boas vindas
 clear
 echo -e "$seta ${blue}Bem vindo a terceira parte da instalação!${end}"
 sleep 2s
 clear
 
+# Atualizando os espelhos
 echo -e "${seta} ${blue}Atualizando os repositórios${end}"
 echo $senha | sudo -S pacman -Syu --noconfirm
 sleep 2s
 clear
 
-# Drivers de video
-echo -e "$seta ${blue}Instalando os drivers de vídeo${end}"
-echo $senha | sudo -S pacman -S nvidia nvidia-utils intel-ucode --noconfirm
-sleep 2s
+echo -en "${seta} ${blue}Digite${end} ${red}[ 1 ]${end} ${blue}para instalar o driver para o virt-manager ou${end} ${red}[ 2 ]${end} ${blue}para instalar o driver nvidia:${end} "
+read resposta
 clear
 
-echo -e "$seta ${blue}Instalando o cinnamon desktop${end}"
-echo $senha | sudo -S pacman -S cinnamon cinnamon-translations cinnamon-screensaver cinnamon-session cinnamon-settings-daemon --noconfirm
-sleep 2s
-clear
+if [ "$resposta" -eq 1 ]; then
+    echo -e "${seta} ${blue}Iniciando instalação do driver para virt-manager${end}"
+    virtmanager
+    sleep 2s
+    clear
+elif [ "$resposta" -eq 2 ]; then
+    echo -e "${seta} ${blue}Iniciando instalação do driver para nvidia${end}"
+    nvidia
+    sleep 2s
+    clear
+else
+    echo -e "${seta} ${red}Resposta inválida!${end}"
+    exit 1
+fi
 
-echo -e "$seta ${blue}Instalando o nemo${end}"
-echo $senha | sudo -S pacman -S nemo nemo-preview nemo-share nemo-fileroller --noconfirm
-sleep 2s
-clear
-
-echo -e "$seta ${blue}Instalando pacotes necessários${end}"
-echo $senha | sudo -S pacman -S accountsservice alsa-utils archlinux-keyring archlinux-wallpaper baobab bash-completion blueberry bluez bluez-cups bluez-tools bolt cmatrix colord coreutils cpio cronie dialog ffmpegthumbnailer gimp gnome-bluetooth gedit gnome-calculator gnome-calendar gnome-disk-utility gnome-keyring gnome-menus gnome-online-accounts gnome-power-manager gnome-screenshot gnome-settings-daemon gnome-system-monitor gnupg gst-libav gufw htop libreoffice libreoffice-fresh-pt-br lollypop man-db metacity mtools neofetch pass rsync system-config-printer tcpdump totem ttf-hack gnu-free-fonts ttf-dejavu ttf-nerd-fonts-symbols ufw unrar xdg-user-dirs xdg-utils xf86-input-synaptics gnome-terminal xcursor-vanilla-dmz-aa xclip xreader youtube-dl --noconfirm
-sleep 2s
-clear
-
-echo -e "${seta} ${blue}Instalando os pacotes necessários${end}"
-echo $senha | sudo -S pacman -S qemu virt-manager dnsmasq libvirt ebtables bridge-utils edk2-ovmf spice-vdagent --noconfirm
-sleep 2s
-clear
-
-echo -e "${seta} ${blue}Iniciando o daemon libvirt${end}"
-echo $senha | sudo -S systemctl enable libvirtd.service
-echo $senha | sudo -S systemctl start libvirtd.service
-sleep 2s
-clear
-
-echo -e "${seta} ${blue}Inserindo $USER no grupo libvirt${end}"
-echo $senha | sudo -S usermod -aG libvirt $USER
-sleep 2s
-clear
-
-echo -e "${seta} ${blue}Para que o suporte de rede seja iniciado automáticamente${end}"
-echo $senha | sudo -S virsh net-autostart --network default
+echo -e "$seta ${blue}Instalando o deepin desktop${end}"
+echo $senha | sudo -S pacman -S deepin deepin-extra --noconfirm
 sleep 2s
 clear
 
@@ -66,11 +91,6 @@ echo -e "$seta ${blue}Instalando o yay${end}"
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
-sleep 2s
-clear
-
-echo -e "$seta ${blue}Instalando o kid3-cli para o lollypop${end}"
-yay -S --noconfirm kid3-cli
 sleep 2s
 clear
 
@@ -84,39 +104,8 @@ yay -S --noconfirm debtap
 sleep 2s
 clear
 
-echo -e "$seta ${blue}Instalando gnome-terminal-transparency${end}"
-yay -S --noconfirm gnome-terminal-transparency
-sleep 2s
-clear
-
-echo -e "$seta ${blue}Instalando cinnamon-sound-effects${end}"
-yay -S --noconfirm cinnamon-sound-effects
-sleep 2s
-clear
-
-echo -e "$seta ${blue}Instalando spotify${end}"
-yay -S --noconfirm spotify
-sleep 2s
-clear
-
 echo -e "$seta ${blue}Instalando o xviewer${end}"
 yay -S --noconfirm xviewer xviewer-plugins
-sleep 2s
-clear
-
-echo -e "$seta ${blue}Instalando as fontes${end}"
-yay -S --noconfirm ttf-ms-fonts
-yay -S --noconfirm ttf-ubuntu-font-family
-sleep 2s
-clear
-
-echo -e "$seta ${blue}Instalando o mint-themes${end}"
-yay -S --noconfirm mint-themes mint-y-icons mintlocale
-sleep 2s
-clear
-
-echo -e "$seta ${blue}Instalando o gnome-online-accounts-git${end}"
-yay -S --noconfirm gnome-online-accounts-git
 sleep 2s
 clear
 
@@ -138,9 +127,33 @@ else
     continue 
 fi
 
-echo -e "$seta ${blue}Instalando e iniciando o gdm${end}"
+echo -e "${seta} ${blue}Instalando o lightdm${end}"
+instalar_lightdm
 sleep 2s
-echo $senha | sudo -S pacman -S gdm --noconfirm
-echo $senha | sudo -S systemctl enable gdm
-echo $senha | sudo -S systemctl start gdm
+clear
 
+
+echo -e "${seta} ${blue}Instalando o lightdm-deepin-greeter${end}"
+deepin_greeter
+sleep 2s
+clear
+
+echo -e "${seta} ${blue}Instalando o lightdm-gtk-greeter${end}"
+gtk_greeter
+sleep 2s
+clear
+
+echo -e "${seta} ${blue}Configurando o lightdm lightdm.conf${end}"
+arquivo_de_configuracao
+sleep 2s
+clear
+
+echo -e "${seta} ${blue}Testando o lightdm com o testmode${end}"
+iniciar_testmode
+sleep 2s
+clear
+
+echo -e "${seta} ${blue}Iniciando o serviço do lightdm${end}"
+iniciar_servico
+sleep 2s
+clear
